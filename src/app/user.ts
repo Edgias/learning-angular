d112f24606405fdffa772a75c354d22e00d365ab
Edgias Mudzukwa <mudzukwae@outlook.com>
2025-12-10 08:24:53 +0200

    input/output properties

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, input, output } from "@angular/core";
 
 @Component({
   selector: 'app-user',
-  template: `{{ userName }}`
+  template: `{{ userName }} 🎉: {{ occupation() }} <br>
+             Years of Experience: {{ yearsOfExperience }} <br>
+             <button (click)="incrementYearsOfExperience()">Gain Experience</button>
+            `
 })
 export class User {
     userName: string = 'Jemimah Aita';
+    occupation = input<string>();
+    yearsOfExperience: number = 0;
+    readonly incrementYearsOfExperienceEvent = output<number>();
+
+    incrementYearsOfExperience() {
+        this.yearsOfExperience++;
+        this.incrementYearsOfExperienceEvent.emit(this.yearsOfExperience);
+    }
 }
